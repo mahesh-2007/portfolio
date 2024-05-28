87aefd89f8678ee81031956a4755a10a079b1f2a
Mahesh-2007 <mm2472229@gmail.com>
2024-05-28 23:14:13 +0530

    js-navbar

---
+++ script.js
@@ -0,0 +1,14 @@
+var a = document.querySelector(".nav2")
+var b = document.querySelector(".fa-x")
+var c = document.querySelector(".fa-bars")
+function bars(){
+a.style.display="flex";
+b.style.display="flex";
+c.style.display="none";
+}
+function barc(){
+a.style.display="none";
+b.style.display="none";
+c.style.display="flex";
+}
+
