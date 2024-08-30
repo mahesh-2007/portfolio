31b3f9f243303879f9e0c5dded4ed0aa11aa959a
Mahesh-2007 <mm2472229@gmail.com>
2024-08-30 20:37:46 +0530

    loader totally fixed

--- script.js
+++ script.js
@@ -12,8 +12,21 @@ b.style.display="none";
 c.style.display="flex";
 }
 var typed = new Typed('.auto-type', {
-    strings: ["Mahesh"],
+    strings: ["PROGRAMMER" , "WEB DEVELOPER"],
     typeSpeed: 150,
     backSpeed: 150,
-   loop: 1000000000000000000000000000
+   loop: 1
   });
+  document.addEventListener("DOMContentLoaded", function() {
+    // Ensure that the body display property is set to block after hiding the loader
+    document.getElementById("loader").style.display = "none";
+    document.body.style.display = "block";  // Ensure this line is reached
+});
+window.addEventListener('load', function () {
+  const loader = document.getElementById('loader');
+  const content = document.getElementById('content');
+
+  // Hide the loader and show the content
+  loader.style.display = 'none';
+  content.style.display = 'block';
+});
